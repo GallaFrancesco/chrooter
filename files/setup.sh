9fc05c5d6441569acced6d5fc0a226978d08b861
Francesco Galla` <galla@di.unito.it>
2019-06-14 14:45:47 +0200

    setup gspn from chroot

--- files/setup.sh
+++ files/setup.sh
@@ -7,6 +7,8 @@ echo "---> this script is run as root from inside the chroot jail."
 echo ""
 echo "[CHROOT] Syncing portage tree"
 emerge --sync
+echo "[CHROOT] Updating packages if needed"
+emerge --update --newuse --with-bdeps=y --deep --keep-going @world
 
 echo "[CHROOT] Installing layman"
 if [[ $(emerge -p --quiet layman 2>/dev/null | grep -i layman | grep -i ebuild | cut -d" " -f4) != "R" ]]; then
@@ -20,22 +22,19 @@ if [[ -z $(layman -l | grep -i greatspn-overlay) ]]; then
 	layman -k -o https://raw.githubusercontent.com/GallaFrancesco/greatspn-overlay/master/greatspn-overlay.xml -f -a greatspn-overlay
 	layman-updater -R
 else
-	echo "[CHROOT] greatspn-overlay already present"
+	echo "[CHROOT] greatspn-overlay already present, syncing"
+	layman -s greatspn-overlay
 fi
 
 echo "[CHROOT] Installing GreatSPN"
-if [[ $(emerge -p --quiet greatspn 2>/dev/null | grep -i greatspn | grep -i ebuild | cut -d" " -f4) != "R" ]]; then
-	emerge greatspn
-else
-	echo "[CHROOT] Skipping since GreatSPN is already installed"
-fi
+emerge greatspn
 
 echo "[CHROOT] Cleaning up"
 if [[ $(emerge -p --quiet gentoolkit 2>/dev/null | grep -i gentoolkit | grep -i ebuild | cut -d" " -f4) != "R" ]]; then
 	emerge gentoolkit
 fi
 
-emerge --deep --depclean --with-bdeps=n
+emerge --deep --depclean --with-bdeps=y
 eclean --deep distfiles
 eclean --deep packages
 rm -rf /usr/portage/distfiles/*
